6a904eff900610ce5b1e93278b78b7ecc6cc9ae6
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-06-12 14:10:25 -0300

    funcao adicionar, exibir receitas

--- omma-js/index.js
+++ omma-js/index.js
@@ -1,6 +1,7 @@
-console.log("Omma - receita com afeto");
+console.log("Omma - receita com afeto\n");
 
-let receita = {
+let receita =
+{
     titulo: "Bolo de cenoura",
     nivelDificuldade: "fácil",
     listaIngrediente: ["cenoura", "farinha de trigo", "ovos", "leite"],
@@ -9,4 +10,82 @@ let receita = {
     vegana: false
 };
 
-console.log(receita.titulo);
+let receitas = [
+
+    {
+        titulo: "Bolo de cenoura",
+        nivelDificuldade: "fácil",
+        listaIngrediente: ["cenoura", "farinha de trigo", "ovos", "leite"],
+        preparo: "Misture tudo e coloque numa forma untada para assar a 200 °C",
+        linkVideo: "http://youtube.com",
+        vegana: false
+    },
+    {
+        titulo: "Tabule",
+        nivelDificuldade: "fácil",
+        listaIngrediente: ["trigo hidratado", "tomate", "pepino", "suco de limão"],
+        preparo: "Corte tudo bem pequeno e misture com suco de limão e azeite",
+        linkVideo: "http://youtube.com",
+        vegana: true
+
+    }
+];
+
+//console.log(receitas.titulo);
+
+const cadastrarReceita = (titulo, nivelDificuldade, listaIngrediente, preparo, linkVideo, vegana) => {
+    let novaReceita = {
+        titulo: titulo,
+        nivelDificuldade: nivelDificuldade,
+        listaIngrediente: listaIngrediente,
+        preparo: preparo,
+        linkVideo: linkVideo,
+        vegana: vegana
+    };
+
+    receitas.push(novaReceita);
+    console.log(`Nova receita cadastrada:  ${novaReceita.titulo}`);
+}
+
+cadastrarReceita("Misto Quente",
+    "fácil",
+    ["pão", "queijo", "presunto"],
+    "Junta tudo e esquenta",
+
+    false
+);
+
+cadastrarReceita("Pastel de Carne",
+    "fácil",
+    ["carne moida, massa de pastel"],
+    "Coloque a carne dentro do pastel e feche, após fechar frite em oléo quente",
+    "htpp://youtube.com",
+    false
+);
+
+
+function exibirReceita() {
+    receitas.forEach(receita => {
+        console.log(`
+            Título: ${receita.titulo}
+            Nível de Dificuldade: ${receita.nivelDificuldade}
+            Ingredientes: ${receita.listaIngrediente.join(", ")}
+            Modo de Preparo: ${receita.preparo}
+            Link do Vídeo: ${receita.linkVideo}
+            Vegana: ${receita.vegana ? 'sim' : 'não'}
+            `);
+    });
+}
+
+const removerReceita = (tituloReceita) => {
+    receitas.filter(tituloReceita => receita.titulo.indexOf)
+    console.log(tituloReceita.indexOf);
+}
+
+
+exibirReceita();
+removerReceita("Misto Quente");
+
+
+
+
