e6c863873a6c9b3b31e4d811ff3e5af4aa1f27f7
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-06-12 11:33:12 -0300

    Testando informações do JS

---
+++ omma-js/index.js
@@ -0,0 +1,12 @@
+console.log("Omma - receita com afeto");
+
+let receita = {
+    titulo: "Bolo de cenoura",
+    nivelDificuldade: "fácil",
+    listaIngrediente: ["cenoura", "farinha de trigo", "ovos", "leite"],
+    preparo: "Misture tudo e coloque numa forma untada para assar a 200 °C",
+    linkVideo: "http://youtube.com",
+    vegana: false
+};
+
+console.log(receita.titulo);
